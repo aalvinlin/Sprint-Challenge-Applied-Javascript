05043884013b3f333572127395b138057f727b1a
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2019-12-06 10:40:20 -0800

    Carousel working with no animations yet

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,60 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let images = "mountains.jpeg computer.jpeg trees.jpeg turntable.jpeg".split(" ");
+let imgIndex = 0;
+
+function createCarousel(images) {
+
+  let parent = document.querySelector(".carousel-container");
+  let carousel = createAndAppend("div", parent, {class: "carousel"});
+    createAndAppend("div", carousel, {class: "left-button", textContent: " < "});
+    
+    for (imgId in images)
+      {
+          let img = images[imgId];
+          let imgClass = "img";
+
+          if (imgId === "0")
+            { imgClass = "img displayed"; }
+
+          createAndAppend("img", carousel, {class: imgClass, id: "img" + imgId, src: "./assets/carousel/" + img});
+      }
+
+    createAndAppend("div", carousel, {class: "right-button", textContent: " > "});
+}
+
+createCarousel(images);
+
+// create left button and right button functionality
+let leftButton = document.querySelector(".left-button");
+leftButton.addEventListener("click", () => {
+  imgIndex -= 1;
+
+  if (imgIndex < 0)
+    { imgIndex = images.length - 1; }
+
+  setImage(imgIndex);
+})
+
+let rightButton = document.querySelector(".right-button");
+rightButton.addEventListener("click", () => {
+  imgIndex += 1;
+
+  if (imgIndex > images.length - 1)
+    { imgIndex = 0; }
+
+    setImage(imgIndex);
+})
+
+function setImage(imgId)
+{
+  let allImages = document.querySelectorAll(".carousel img");
+  allImages.forEach(img => {img.classList.remove("displayed");});
+
+  let img = document.getElementById("img" + imgId);
+  img.classList.add("displayed");
+
+}
+
